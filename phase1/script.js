d89484ac7f1208ff7ea21f1fb91c1cd15bd24e5f
ahmedmustafa48 <ahmedcodes48@gmail.com>
2025-07-02 15:07:54 +0500

    Add initial JavaScript file with variable declaration examples and explanations

---
+++ phase1/script.js
@@ -0,0 +1,16 @@
+// var , let , const -- line by line comparison
+// var = ES5
+// let , const = ES6
+
+// ================= declaration and initialization =================
+
+var a; // declaration
+var a = 12; // declaration and initialization
+
+// var problems
+//  1. window mein add hota hai
+//  2. function scoped hota hai
+//  3. redeclaration bhi kr skty ho same name se and error bhi nhi ayega
+
+var a = 12;
+var a = 13;
