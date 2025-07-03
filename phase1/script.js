aa9f3e7d5538307cbb7b14916bd968f6a12d42ef
ahmedmustafa48 <ahmedcodes48@gmail.com>
2025-07-03 12:51:59 +0500

    Update script.js with examples of variable redeclaration and reassignment, including explanations of 'let' and temporal dead zone.

--- phase1/script.js
+++ phase1/script.js
@@ -11,6 +11,14 @@ var a = 12; // declaration and initialization
 //  1. window mein add hota hai
 //  2. function scoped hota hai
 //  3. redeclaration bhi kr skty ho same name se and error bhi nhi ayega
+// for example
+// var a = 12;
+// var a = 13;
 
-var a = 12;
-var a = 13;
+// LET is reassignable but not redeclareable
+
+let b = 12;
+b = 13;
+console.log(b);
+
+console.log("========== Lets talk about temporal dead zone ");
